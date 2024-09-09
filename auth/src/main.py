4ede94f2ae5ae798fb83557dd3299abbc909c090
Dmytro Sydoruk <dima.sudoryk@gmail.com>
2024-09-09 21:43:10 +0300

    add Auth Service

---
+++ auth/src/main.py
@@ -0,0 +1,32 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.common import constants, logger, seeders, database
+
+from src.modules.customers.router import router as customer_router
+from src.modules.auth.router import router as auth_router
+
+
+@asynccontextmanager
+async def lifespan(app):
+    logger.init_logger()
+    await database.create_tables_postgres()
+    await seeders.run_seeders()
+
+    yield
+
+
+app = FastAPI(title="Auth Service", version="1", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[constants.GATEWAY_HOST, constants.SITE_HOST],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router, tags=["Auth"])
+app.include_router(customer_router, tags=["Customer"])
